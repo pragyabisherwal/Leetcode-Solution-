e2502691a6fab58455008db84f065ff100f17be4
PRAGYA BISHERWAL <90054472+pragyabisherwal@users.noreply.github.com>
2022-06-10 12:32:24 +0530

    Time: 0 ms (100.00%), Space: 41.7 MB (38.09%) - LeetHub

---
+++ 100-same-tree/100-same-tree.java
@@ -0,0 +1,64 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public boolean helper(TreeNode p, TreeNode q)
+    {
+        boolean ans = false;
+        
+        if(p==null && q==null)
+        {
+            ans=true;
+            return ans;
+        }
+        
+        if(p==null && q!=null || p!=null && q==null)
+        {
+            return ans;
+        }
+        
+        if(p.val!=q.val)
+        {
+            return ans;
+        }
+            
+        boolean left=helper(p.left , q.left);
+        boolean right=helper(p.right , q.right);
+        
+        ans = left&&right;
+        
+        return ans;
+        
+        
+    }
+    public boolean isSameTree(TreeNode p, TreeNode q) 
+    {
+        if(p==null && q==null)
+            return true;
+        
+        else if(p==null && q!=null)
+            return false;
+        
+        else if(p!=null && q==null)
+            return false;
+        
+        if(p.val!=q.val)
+            return false;
+        
+        boolean result =helper(p,q);
+        return result;
+        
+    }
+}
