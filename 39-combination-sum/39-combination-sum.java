4a15d4545d431bce22e62a8739763f683a10940b
PRAGYA BISHERWAL <90054472+pragyabisherwal@users.noreply.github.com>
2022-06-15 07:43:59 +0530

    Time: 6 ms (56.06%), Space: 45.7 MB (23.96%) - LeetHub

---
+++ 39-combination-sum/39-combination-sum.java
@@ -0,0 +1,29 @@
+class Solution {
+    public void solve(int src ,int target, List<Integer> temp ,  List<List<Integer>> li , int[] candidates)
+    {
+        if(target==0)
+        {
+            li.add(new ArrayList(temp));
+        }
+        
+        if(target<0)
+            return;
+        
+        for(int i=src; i<candidates.length; i++)
+        {
+            temp.add(candidates[i]);
+            solve(i,target-candidates[i], temp ,li,candidates);
+            temp.remove(temp.size()-1);
+        }
+        
+    }
+    public List<List<Integer>> combinationSum(int[] candidates, int target) 
+    {
+        List<List<Integer>> li = new ArrayList<> ();
+        List<Integer> temp = new ArrayList<> ();
+        
+        solve(0,target ,temp,li,candidates);
+        return li;
+        
+    }
+}
