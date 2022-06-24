e4c914adb7a26083e0f9732c17857fdcd470db83
PRAGYA BISHERWAL <90054472+pragyabisherwal@users.noreply.github.com>
2022-06-24 12:20:17 +0530

    Time: 82 ms (62.36%), Space: 74.1 MB (49.51%) - LeetHub

---
+++ 493-reverse-pairs/493-reverse-pairs.java
@@ -0,0 +1,39 @@
+class Solution {
+    public static int ans;
+    
+    public static int reversePairs(int[] nums) {
+        ans=0;
+        helper(nums , 0 , nums.length-1); 
+        return ans;
+        
+    }
+    
+    public static void helper(int [] nums , int lo , int hi)
+    {
+       if(lo<hi)
+        {
+            int mid = lo + (hi-lo)/2;
+            helper(nums , lo , mid);
+            helper(nums,mid+1 , hi);
+            
+            int i=lo;
+            int j=mid+1;
+            
+           while(i<=mid && j<=hi)
+           {
+               if((long)nums[i] > (long)2*nums[j])
+               {
+                   j++;
+                   ans+=mid-i+1;
+               }
+               
+               else
+               {
+                   i++;
+               }
+           }
+            
+            Arrays.sort(nums , lo , hi+1);
+        }
+    }
+}
