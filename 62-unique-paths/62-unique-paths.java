a427aa04c51d4df6b8995420cc0d63cf69b1ae57
PRAGYA BISHERWAL <90054472+pragyabisherwal@users.noreply.github.com>
2022-06-24 06:56:49 +0530

    Time: 0 ms (100.00%), Space: 39.1 MB (91.57%) - LeetHub

--- 62-unique-paths/62-unique-paths.java
+++ 62-unique-paths/62-unique-paths.java
@@ -1,49 +1,35 @@
 class Solution {
     
-    public static boolean inRange(int i, int j, int n, int m) {
-        if (i < 0 || i >= n || j < 0 || j >= m)
-            return false;
-        return true;
-    }
+    static int [][] dir = {{1 , 0} , {0 , 1}};
     
-    public static int [][] dir={{1,0},{0,1}};
-    
-    public int helper(int sr , int sc , int dr , int dc , int[][]dp)
+    public int helper (int sr , int sc , int dr , int dc , int[][]dp)
     {
         if(sr==dr && sc==dc)
-        {
-            return dp[sr][sc]=1;
-        }
+            return 1;
         
         if(dp[sr][sc]!=0)
-        {
             return dp[sr][sc];
-        }
         
         int count=0;
         
-        for(int d=0 ; d<dir.length;d++)
+        for(int d=0 ; d<dir.length ; d++)
         {
             int row = sr + dir[d][0];
-            int col=sc + dir[d][1];
+            int col = sc + dir[d][1];
             
-            if(inRange(row , col , dr+1,dc+1))
-             count+=helper(row , col, dr , dc,dp);
+            if(row<= dr && col<=dc && row>=0 && col>=0)
+            {
+               count+=helper( row , col , dr , dc , dp); 
+            }
+            
         }
         
-       return dp[sr][sc]=count;
-        
-        
+        return dp[sr][sc]=count;
     }
     
     public int uniquePaths(int m, int n) 
     {
-        int [][]dp = new int[m][n];
-        
-        int result=helper(0,0,m-1,n-1,dp);
-        return result;
-        
-        
-        
+        int [][] dp = new int[m][n];
+        return helper( 0 , 0 , m-1 , n-1 , dp);
     }
 }
