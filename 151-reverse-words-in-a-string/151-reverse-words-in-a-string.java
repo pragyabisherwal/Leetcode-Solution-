34cb7b02cbc5b2dd09119de6c299076dc7e1c687
PRAGYA BISHERWAL <90054472+pragyabisherwal@users.noreply.github.com>
2022-06-24 20:14:57 +0530

    Time: 42 ms (5.14%), Space: 54 MB (21.00%) - LeetHub

--- 151-reverse-words-in-a-string/151-reverse-words-in-a-string.java
+++ 151-reverse-words-in-a-string/151-reverse-words-in-a-string.java
@@ -1,38 +1,71 @@
+// class Solution {
+//     public String reverseWords(String s) 
+//     {
+//         Stack <Character> st = new Stack();
+//         String result="";
+        
+        
+//         for(int i=s.length()-1; i>=0 ; i--)
+//         {
+//             if(s.charAt(i)!=' ')
+//             {
+//                 st.push(s.charAt(i));
+//             }
+            
+//             else if(s.charAt(i)==' ' && st.size()>0)
+//             {
+//                 while(st.size()>0)
+//                 {
+//                    result+=st.pop();
+//                 }
+                
+//                 result+=' ';
+//             }
+            
+            
+//         }
+        
+//          while(st.size()>0)
+//         {
+//           result+=st.pop();
+//          }
+        
+        
+        
+//         return result.trim();
+        
+//     }
+// }
+
+
+
+
+
 class Solution {
     public String reverseWords(String s) 
     {
-        Stack <Character> st = new Stack();
+        s=s.trim();
+        s=s+" ";
+        
+        String word="";
+        
         String result="";
         
-        
-        for(int i=s.length()-1; i>=0 ; i--)
+        for(int i=0;i<s.length();i++)
         {
+            
+            if(i!=s.length()-1 &&s.charAt(i+1)==' '&&s.charAt(i)==' ')
+                continue;
+            
             if(s.charAt(i)!=' ')
-            {
-                st.push(s.charAt(i));
-            }
+                word=word+s.charAt(i);
             
-            else if(s.charAt(i)==' ' && st.size()>0)
-            {
-                while(st.size()>0)
-                {
-                   result+=st.pop();
-                }
-                
-                result+=' ';
+            else{
+                result=word+" "+result;
+                word="";
             }
-            
             
         }
-        
-         while(st.size()>0)
-        {
-          result+=st.pop();
-         }
-        
-        
-        
         return result.trim();
-        
     }
 }
