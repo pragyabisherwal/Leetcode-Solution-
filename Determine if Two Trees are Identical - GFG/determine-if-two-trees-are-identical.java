c6092b240a22263fad3043113057ea2bd061b5f7
PRAGYA BISHERWAL <90054472+pragyabisherwal@users.noreply.github.com>
2022-06-27 12:59:51 +0530

    Added solution - LeetHub

--- Determine if Two Trees are Identical - GFG/determine-if-two-trees-are-identical.java
+++ Determine if Two Trees are Identical - GFG/determine-if-two-trees-are-identical.java
@@ -153,18 +153,7 @@ class Solution
     }
 	boolean isIdentical(Node root1, Node root2)
 	{
-	   if(root1==null && root2==null)
-	    return true;
-	    
-	   if(root1!=null && root2==null)
-	    return false;
-	    
-	   if(root1==null && root2!=null)
-	    return false;
-	    
-	    if(root1.data!=root2.data)
-	     return false;
-	     
+	 
 	    return helper(root1 , root2);
 	}
 	
