f830a6ebb0e6bfae5dff7e923533461139177c60
PRAGYA BISHERWAL <90054472+pragyabisherwal@users.noreply.github.com>
2022-06-27 12:00:31 +0530

    Time: 2 ms (34.24%), Space: 43.2 MB (12.00%) - LeetHub

---
+++ 103-binary-tree-zigzag-level-order-traversal/103-binary-tree-zigzag-level-order-traversal.java
@@ -0,0 +1,80 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public List<List<Integer>> zigzagLevelOrder(TreeNode root) {
+        
+        List<List<Integer>> al = new ArrayList<> ();
+        if(root==null)
+            return al;
+        
+        
+        Stack <TreeNode> ms = new Stack<> ();
+        ms.push(root);
+        
+        Stack <TreeNode> cs = new Stack<> ();
+        int level=1;
+        
+        while(ms.size()!=0)
+        {
+            List<Integer> li = new ArrayList<>();
+            int size=ms.size();
+            
+            for(int i=0 ; i<size ; i++)
+            {
+               TreeNode rem = ms.pop();
+               li.add(rem.val);
+            
+                if(level%2==0)
+                {
+                    if(rem.right!=null)
+                      {
+                          cs.push(rem.right);
+                      }  
+                       if(rem.left!=null)
+                       {
+                           cs.push(rem.left);
+                       }   
+                } 
+                
+                else
+                {
+                     if(rem.left!=null)
+                       {
+                           cs.push(rem.left);
+                       }
+                      if(rem.right!=null)
+                      {
+                          cs.push(rem.right);
+                      }  
+                }
+            }
+            
+            if(ms.size()==0)
+            {
+                ms=cs;
+                cs = new Stack ();
+                level++;
+                
+            }
+            
+            al.add(li);
+            
+        }
+        
+        return al;
+        
+    }
+}
