c7dc5fe2062f39cd60d9178435e499d54756ba70
PRAGYA BISHERWAL <90054472+pragyabisherwal@users.noreply.github.com>
2022-07-16 16:40:43 +0530

    Time: 4 ms (69.63%), Space: 51 MB (10.67%) - LeetHub

--- 4-median-of-two-sorted-arrays/4-median-of-two-sorted-arrays.java
+++ 4-median-of-two-sorted-arrays/4-median-of-two-sorted-arrays.java
@@ -17,25 +17,17 @@ class Solution {
         int lo=0;
         int hi=n1;
         
-        double l1;
-        double l2;
-        double r1;
-        double r2;
-        
-        int cut1;
-        int cut2;
-        
         while(lo<=hi)
         {
-             cut1=lo+((hi-lo)/2);
-             cut2=(n1+n2)/2-cut1;
+            int cut1=lo+((hi-lo)/2);
+            int cut2=(n1+n2)/2-cut1;
             
-             l1 = cut1==0?Integer.MIN_VALUE : nums1[cut1-1];
-             l2 = cut2==0?Integer.MIN_VALUE : nums2[cut2-1];
+             double l1 = cut1==0?Integer.MIN_VALUE : nums1[cut1-1];
+             double l2 = cut2==0?Integer.MIN_VALUE : nums2[cut2-1];
             
             
-             r1 = cut1==n1?Integer.MAX_VALUE : nums1[cut1];
-             r2 = cut2==n2?Integer.MAX_VALUE : nums2[cut2];
+             double r1 = cut1==n1?Integer.MAX_VALUE : nums1[cut1];
+             double r2 = cut2==n2?Integer.MAX_VALUE : nums2[cut2];
             
             if(l1>r2)
                 hi=cut1-1;
