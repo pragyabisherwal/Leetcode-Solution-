7baaba41f07329a6af685c854e5587d218e5c058
PRAGYA BISHERWAL <90054472+pragyabisherwal@users.noreply.github.com>
2022-08-01 11:42:31 +0530

    Time: 0 ms (100.00%), Space: 43.7 MB (63.27%) - LeetHub

--- 98-validate-binary-search-tree/98-validate-binary-search-tree.java
+++ 98-validate-binary-search-tree/98-validate-binary-search-tree.java
@@ -17,12 +17,18 @@ class Solution {
      long prev = Long.MIN_VALUE;
     public boolean isValidBST(TreeNode root) 
     {
+        //Inorder Traversal Of a BST is sorted.
         if(root==null)
             return true;
      
+        //Left call
         boolean leftAns=isValidBST(root.left);
         if(leftAns==false)
              return false;
+        
+        //Inorder we will check that it should not be greater than the prev one because 
+        //than it will not be sorted and hence do not satisy the condition of BST and 
+        //return false else just update the prev value with the current one
         
         if(prev >= root.val)
             return false;
