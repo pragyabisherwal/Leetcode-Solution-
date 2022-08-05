5d999150036e9ea5048a1814ea786cc398432437
PRAGYA BISHERWAL <90054472+pragyabisherwal@users.noreply.github.com>
2022-08-05 12:03:26 +0530

    Time: 0 ms (100.00%), Space: 40.1 MB (84.05%) - LeetHub

--- 509-fibonacci-number/509-fibonacci-number.java
+++ 509-fibonacci-number/509-fibonacci-number.java
@@ -1,28 +1,22 @@
 class Solution {
-    public void solve(int curr,int n , int [] dp)
-    {
-        if(curr==dp.length)
-        {
-            return;
-        }
-        
-        dp[curr]=dp[curr-1] + dp[curr-2];
-       
-        solve(curr+1 , n , dp );
-        
-    }
+    
     public int fib(int n) 
     {
-        if(n==0)
-            return 0;
-        else if(n==1)
-            return 1;
+        if(n==0 || n==1)
+            return n;
         
         int [] dp = new int[n+1];
-        dp[0]=0;
-        dp[1]=1;
         
-        solve(2 , n , dp);
+        for(int i=0 ; i <dp.length; i++)
+        {
+            if(i==0 || i==1)
+                dp[i]=i;
+            
+            else
+                dp[i]=dp[i-1]+dp[i-2];
+        }
+       
+      
         return dp[n];
         
     }
