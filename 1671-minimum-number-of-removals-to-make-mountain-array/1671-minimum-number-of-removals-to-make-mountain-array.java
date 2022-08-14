ac204ad255000fcfa5fe5109a66a5ae7206bff6f
PRAGYA BISHERWAL <90054472+pragyabisherwal@users.noreply.github.com>
2022-08-14 19:38:30 +0530

    Time: 64 ms (67.16%), Space: 46.2 MB (64.42%) - LeetHub

--- 1671-minimum-number-of-removals-to-make-mountain-array/1671-minimum-number-of-removals-to-make-mountain-array.java
+++ 1671-minimum-number-of-removals-to-make-mountain-array/1671-minimum-number-of-removals-to-make-mountain-array.java
@@ -1,37 +1,54 @@
 class Solution {
-// Implementation of Longest Bitonic Subsequence
-    public int minimumMountainRemovals(int[] nums) {
+    public int minimumMountainRemovals(int[] nums) 
+    {
         int n = nums.length;
-        int [] lis = new int[n]; // longest Increasing Subsequece Array
-        int [] lds = new int[n]; // longest Decreasing Subsequece Array
         
-//     lis -> length of longest Increasing Subsequnce starting from 0th idx till ith idx
-        for(int i = 0; i < n; i++){
-            int max = 0;
-            for(int j = 0; j < i; j++){
-                if(nums[j] < nums[i]){
-                    max = Math.max(max, lis[j]);
+        int [] LIS = new int[n];
+        
+        for(int i=0;i<n ; i++)
+        {
+            int max=0;
+            for(int j=0 ; j<i; j++)
+            {
+                if(nums[j]<nums[i])
+                {
+                 if(max<LIS[j])
+                    max=LIS[j];
                 }
+               
             }
-            lis[i] = max + 1;
+            
+            LIS[i]=max+1;
         }
         
-//   lds -> length of longest Decreasing Subsequence starting from ith idx till end
-        for(int i = n - 1; i >= 0; i--){ 
-            int max = 0;
-            for(int j = i + 1; j < n; j++){
-                if(nums[j] < nums[i]){
-                    max = Math.max(max, lds[j]);
+        
+        int [] LDS = new int[n];
+        for(int i=n-1; i>=0 ; i--)
+        {
+            int max=0;
+            for(int j=i+1;j<n ; j++)
+            {
+                if(nums[j]<nums[i])
+                {
+                 if(max<LDS[j])
+                    max=LDS[j];
                 }
+               
             }
-            lds[i] = max + 1;
+            
+            LDS[i]=max+1;
         }
         
-        int max = 0;
-        for(int i = 0; i < n; i++){
-            if(lis[i] > 1 && lds[i] > 1) // if this ith elemenet is single then dont consider in the longest bitonic subsequence
-            max = Math.max(max, lis[i] + lds[i]);
+        
+        
+        int overAllmax = Integer.MIN_VALUE;
+        for(int i=0 ; i<n ; i++)
+        {
+            if(LIS[i] >1 &&    LDS[i]> 1)
+                overAllmax = Math.max(overAllmax , LIS[i]+LDS[i]);
         }
-        return n - (max - 1);
+        
+        return n-(overAllmax-1);   
+        
     }
 }
