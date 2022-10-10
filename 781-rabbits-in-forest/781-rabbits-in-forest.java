775b181430b6299acca182095b68efa6904f3aa7
PRAGYA BISHERWAL <90054472+pragyabisherwal@users.noreply.github.com>
2022-10-10 09:22:11 +0530

    Time: 6 ms (44.96%), Space: 42.4 MB (69.36%) - LeetHub

---
+++ 781-rabbits-in-forest/781-rabbits-in-forest.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int numRabbits(int[] answers) {
+        int map[] =  new int[1000];
+        
+        for(int ele : answers){
+            map[ele]+=1;
+        }
+        int rabbit =  0;
+        
+        for(int i=0;i<1000;i++){
+            int grp  = i+1;
+            int c =  map[i];
+            int count = (int) Math.ceil( (c*1.0)/grp ) * grp;
+            rabbit+= count;            
+        }
+        return rabbit;
+    }
+}
