f6aada46b9d3057180b2c0ed27d15be008107bff
PRAGYA BISHERWAL <90054472+pragyabisherwal@users.noreply.github.com>
2023-09-07 13:39:30 +0530

    Time: 27 ms (91.33%), Space: 63.1 MB (5.55%) - LeetHub

--- 0239-sliding-window-maximum/0239-sliding-window-maximum.java
+++ 0239-sliding-window-maximum/0239-sliding-window-maximum.java
@@ -1,22 +1,35 @@
 class Solution {
-    public int[] maxSlidingWindow(int[] nums, int k) {
-        int i=0, j=0, res[]=new int[nums.length-k+1], c=0;
-        PriorityQueue<Integer> pq = new PriorityQueue<Integer>((a,b)->b-a);
-        HashMap<Integer, Integer> map = new HashMap<Integer, Integer>();
-        for(;j<nums.length; ++j) {
-            pq.add(nums[j]);
-            if(j-i+1>k){
-                map.put(nums[i], map.getOrDefault(nums[i], 0)+1);
-                ++i;
-            }
-            while(map.containsKey(pq.peek())) {
-                int pop = pq.poll(), f;
-                map.put(pop, (f=map.get(pop)-1));
-                if(f==0) map.remove(pop);
-            }
-            if(j-i+1==k)
-                res[c++] = pq.peek();
+    public int[] maxSlidingWindow(int[] nums, int k)
+    {
+        int n = nums.length;
+        Deque<Integer> qu = new ArrayDeque<Integer>();
+        int i , j=0;
+        int [] res = new int[n-k+1]; 
+        
+        for(i=0 ; i<k ; i++)
+        {
+          while(!qu.isEmpty() && nums[i]>=nums[qu.peekLast()])  
+              qu.removeLast();
+        
+          qu.addLast(i);
         }
+        
+        for(; i<n ; i++)
+        {
+           res[j++]= nums[qu.peek()];
+           
+        
+           while(!qu.isEmpty() && qu.peek()<=i-k)  
+              qu.removeFirst();
+            
+            
+            while(!qu.isEmpty() && nums[i]>=nums[qu.peekLast()])  
+              qu.removeLast();
+            
+            qu.addLast(i);
+        }
+        
+        res[j++]= nums[qu.peek()];
         return res;
     }
 }
