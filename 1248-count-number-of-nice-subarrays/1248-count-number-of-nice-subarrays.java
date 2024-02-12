30f40fa1ea6f3c3c61ae045e29b86767299dcea4
PRAGYA BISHERWAL <90054472+pragyabisherwal@users.noreply.github.com>
2024-02-12 19:13:33 +0530

    Time: 8 ms (77.48%), Space: 54.6 MB (83.32%) - LeetHub

--- 1248-count-number-of-nice-subarrays/1248-count-number-of-nice-subarrays.java
+++ 1248-count-number-of-nice-subarrays/1248-count-number-of-nice-subarrays.java
@@ -1,31 +1,31 @@
 class Solution {
     public int numberOfSubarrays(int[] nums, int k) {
-        int i = 0, j = 0, c = 0, total = 0, count = 0;
-
-        // Iterate through the array using two pointers i and j
-        while (j < nums.length) {
-            // If the current element is odd, increment the count of odd numbers (c)
+        
+        int n = nums.length;
+        int i=0 , j=0 , count=0 ,eveNum=0;
+        int maxNum=0;
+        
+        while(j<n)
+        {
             if (nums[j] % 2 != 0) {
-                c++;
-                count = 0; // Reset the count of subarrays
+                count++;
+                eveNum=0; // Reset the count of subarrays
             }
+           
+            while(count==k)
+            {
+                count = (nums[i] % 2 != 0) ? count - 1 : count;
 
-            // Check if the count of odd numbers is equal to k
-            while (c == k) {
-                // Decrease the count of odd numbers and move the left pointer (i) to the right
-                c = (nums[i] % 2 != 0) ? c - 1 : c;
                 i++;
-                count++; // Increment the count of subarrays
+                eveNum++;  
             }
-
-            // Add the count of subarrays to the total
-            total += count;
-
-            // Move the right pointer (j) to the right
+            
+            maxNum+=eveNum;
             j++;
+            
         }
-
-        // Return the total number of subarrays with at least k odd numbers
-        return total;
+        
+        return maxNum;
+        
     }
 }
