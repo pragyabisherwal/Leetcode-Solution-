a406a3a810582b5543e376a691b6b5cb352c2373
PRAGYA BISHERWAL <90054472+pragyabisherwal@users.noreply.github.com>
2024-02-20 13:07:25 +0530

    Time: 16 ms (48.75%), Space: 46.1 MB (11.46%) - LeetHub

--- 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.java
+++ 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.java
@@ -1,62 +1,27 @@
 class Solution {
-    public class Pair implements Comparable<Pair>
-    {
-        char val;
-        int freq;
-        
-        Pair(char val , int freq)
-        {
-            this.val = val;
-            this.freq = freq;
-        }
-        
-        public int compareTo(Pair other)
-        {
-            if(this.freq==other.freq)
-            {
-                return other.val-this.val;
-            }
-            else
-                return this.freq-other.freq;
-        }
+    public String frequencySort(String s) {
+        String res = sortByFrequencyHelper(s);
+        return res;
     }
-    public String frequencySort(String s) 
-    {
-        HashMap<Character,Integer>hm = new HashMap<>();
-        PriorityQueue<Pair> pq = new PriorityQueue<>(Collections.reverseOrder());
-        
-        for(int i=0 ; i<s.length() ; i++)
-        {
-            char ch = s.charAt(i);
-            hm.put(ch , hm.getOrDefault(ch , 0)+1);
-        }
-        
-        for(char ch: hm.keySet())
-        {
-            int value = hm.get(ch);
-            
-            Pair temp = new Pair(ch , value);
-            
-            pq.add(temp);
-        }
-        
-        String ans= "";
-        int k = 0;
-      
-        
-         while (!pq.isEmpty()) 
-         {
-             Pair temp = pq.remove();
-             char first = temp.val;
-             int second = temp.freq;
-             
-             for(int i=0 ; i<second ; i++)
-              {  
-                 ans+=first;
-              }
-             
-        }
 
-       return ans;
+    private static String sortByFrequencyHelper(String s) {
+        Map<Character, Integer> freq = new HashMap<>();
+        StringBuilder res = new StringBuilder();
+        PriorityQueue<Character> maxHeap = new PriorityQueue<>(
+                (freq1, freq2) -> freq.get(freq2) - freq.get(freq1)
+        );
+        for (char c: s.toCharArray()){
+            freq.put(c, freq.getOrDefault(c, 0) + 1);
+        }
+        for (char key : freq.keySet()){
+            maxHeap.add(key);
+        }
+        while (!maxHeap.isEmpty()){
+            char c = maxHeap.poll();
+            int n = freq.get(c);
+            String temp = new String("" + c).repeat(n);
+            res.append(temp);
+        }
+        return res.toString();
     }
 }
