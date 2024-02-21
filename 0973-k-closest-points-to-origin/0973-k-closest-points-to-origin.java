93836bc390c3b10b9f3380371c47a1cb0535b355
PRAGYA BISHERWAL <90054472+pragyabisherwal@users.noreply.github.com>
2024-02-21 11:51:29 +0530

    Time: 33 ms (46.65%), Space: 50.7 MB (74.67%) - LeetHub

--- 0973-k-closest-points-to-origin/0973-k-closest-points-to-origin.java
+++ 0973-k-closest-points-to-origin/0973-k-closest-points-to-origin.java
@@ -1,23 +1,27 @@
 class Solution {
-    public int[][] kClosest(int[][] points, int k) {
-        
+    public int[][] kClosest(int[][] points, int k) 
+    {
         PriorityQueue<int[]> pq = new PriorityQueue<>(
-        (p1, p2) -> getDistance(p1).compareTo(getDistance(p2)));
-        
-        for(int i = 0; i < points.length; i++) {
+            (p1,p2)->Integer.compare(p2[0]*p2[0]+p2[1]*p2[1],p1[0]*p1[0]+p1[1]*p1[1])
+        );
+            
+        for(int i=0 ; i<points.length ; i++)
+        {
             pq.add(points[i]);
+            
+            if(pq.size()>k)
+                pq.remove();
         }
         
-        int[][] result = new int[k][2];
+        int[][] ans = new int[k][2];
+        k--;
         
-        for(int i = 0; i < k; i++) {
-            result[i] = pq.poll();    
+        while(pq.size()>0)
+        {
+            ans[k]=pq.remove();
+            k--;
         }
         
-        return result;
-    }
-    
-    private Double getDistance(int[] point) {
-        return Math.sqrt((point[0] * point[0]) + (point[1] * point[1]));
+        return ans;
     }
 }
