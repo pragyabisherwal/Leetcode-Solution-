05dd19d36fbbc6e366c41801a8a58d0b0abfc97d
PRAGYA BISHERWAL <90054472+pragyabisherwal@users.noreply.github.com>
2024-02-25 11:47:10 +0530

    Time: 29 ms (56.14%), Space: 62.3 MB (44.54%) - LeetHub

--- 1331-rank-transform-of-an-array/1331-rank-transform-of-an-array.java
+++ 1331-rank-transform-of-an-array/1331-rank-transform-of-an-array.java
@@ -1,25 +1,30 @@
 class Solution {
 	public int[] arrayRankTransform(int[] arr) {
-		//USING PRIORITY QUEUE AND HASHMAP
-		int n= arr.length;
-		PriorityQueue<Integer> pq= new PriorityQueue<>();
-		for(int i=0; i<n; i++)
-		   pq.add(arr[i]);
-		HashMap<Integer,Integer> map= new HashMap<>();
-		int count=1;
-		while(!pq.isEmpty())
-		{
-			int val= pq.remove();
-			if(!map.containsKey(val))
-			{
-				map.put(val,count);
-				count++;
-			} 
-		}
-		for(int i=0; i<n; i++)
-		{
-			arr[i]=map.get(arr[i]);
-		}
-		return arr;
-	}
+	HashMap < Integer, Integer > mp = new HashMap < Integer, Integer > ();
+    int temp = 1;
+    int n = arr.length;
+    
+    int brr[] = new int[n];
+    //Copying elememts from arr to brr
+    for (int i = 0; i < n; i++) {
+      brr[i] = arr[i];
+    }
+    Arrays.sort(brr);
+        
+    for (int i = 0; i < n; i++) {
+      //if element is previously not store in the map
+      if (mp.get(brr[i]) == null) {
+        mp.put(brr[i], temp);
+        temp++;
+      }
+    }
+    
+    for (int i = 0; i < n; i++) {
+      brr[i]= mp.get(arr[i]);
+      }
+        
+    return brr;
+    }
+        
+    
 }
