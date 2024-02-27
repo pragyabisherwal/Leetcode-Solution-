3f9bd8e0937f33592ea78dd301218a1d453c693e
PRAGYA BISHERWAL <90054472+pragyabisherwal@users.noreply.github.com>
2024-02-27 08:54:42 +0530

    Time: 86 ms (97.18%), Space: 63.8 MB (45.13%) - LeetHub

--- 0295-find-median-from-data-stream/0295-find-median-from-data-stream.java
+++ 0295-find-median-from-data-stream/0295-find-median-from-data-stream.java
@@ -1,40 +1,37 @@
 class MedianFinder {
-
-    PriorityQueue<Integer>left;
-    PriorityQueue<Integer>right;
+    PriorityQueue<Integer> left;
+    PriorityQueue<Integer> right;
     
-    public MedianFinder() 
-    {
-        left = new PriorityQueue<> (Collections.reverseOrder());
-        right = new PriorityQueue<> ();       
+    public MedianFinder() {
+        left = new PriorityQueue<>((a,b) -> b-a);
+        right = new PriorityQueue<>();
     }
     
-    public void addNum(int num) 
-    {
-        if(left.size()==0 || left.peek()>=num)
+    public void addNum(int num) {
+        if (left.size() == 0 && right.size() == 0) {
             left.add(num);
-        else
-            right.add(num);
-        
-        if(left.size()-right.size()>1)  //beacuse in case of odd numbers 1 number can be extra in left but if more than 1 number is extra than we have to remove and add in min heap
-            right.add(left.poll());
-        
-        else if(right.size()-left.size()>0)
-            left.add(right.poll());
-            
-        
+        } else if (left.size() > right.size()) {
+            if (num >= left.peek()) {
+                right.add(num);
+            } else {
+                right.add(left.poll());
+                left.add(num);
+            }
+        } else {
+            if (num <= right.peek()) {
+                left.add(num);
+            } else {
+                left.add(right.poll());
+                right.add(num);
+            }
+        }
     }
     
-    public double findMedian() 
-    {
-        if(left.size()==right.size())
-            return left.peek()/2.0 + right.peek()/2.0;
-        
-        return (double)left.peek();
-        
+    public double findMedian() {
+        if (left.size() > right.size()) return left.peek();
+        return (left.peek() + right.peek()) / 2.0;
     }
 }
-
 /**
  * Your MedianFinder object will be instantiated and called as such:
  * MedianFinder obj = new MedianFinder();
