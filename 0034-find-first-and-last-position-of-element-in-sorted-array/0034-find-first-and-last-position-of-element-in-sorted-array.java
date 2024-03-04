39488453f6132ddab238110ea7fae146d0cf8b7e
PRAGYA BISHERWAL <90054472+pragyabisherwal@users.noreply.github.com>
2024-03-04 12:15:30 +0530

    Time: 0 ms (100.00%), Space: 45.7 MB (70.13%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.java
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.java
@@ -1,30 +1,62 @@
 class Solution {
     public int[] searchRange(int[] a, int t) {
         int[] ans={-1,-1};
-        ans[0]=search(a,t,true);
-        if(ans[0]!=-1)ans[1]=search(a,t,false);
+        
+        ans[0]=searchFirst(a,t);
+        ans[1]=searchLast(a,t);
         return ans;
     }
     
-    public static int search(int[] a,int t,boolean firstIndex)
+    public static int searchFirst(int[] a,int t)
     {
-        int s=0;
-        int e=a.length-1;
+        int start=0;
+        int end=a.length-1;
         
         int ans=-1;
-        while(s<=e)
+        
+        while(start<=end)
         {
-            int m=s+(e-s)/2;
+            int mid=start + (end-start)/2;
             
-            if(a[m]>t)
-                e=m-1;
-            else if(a[m]<t)
-                s=m+1;
-            else{
-                ans=m;
-                if(firstIndex)e=m-1;
-                else s=m+1;
+            if(a[mid]==t)
+            {
+                ans = mid;
+                end = mid-1;
             }
+            
+            else if(a[mid]>t)
+                end=mid-1;
+            
+            else 
+                start=mid+1;
+            
+        }
+        return ans;
+    }
+    
+    public static int searchLast(int[] a,int t)
+    {
+        int start=0;
+        int end=a.length-1;
+        
+        int ans=-1;
+        
+        while(start<=end)
+        {
+            int mid=start + (end-start)/2;
+            
+            if(a[mid]==t)
+            {
+                ans = mid;
+                start = mid+1;
+            }
+            
+            else if(a[mid]>t)
+                end=mid-1;
+            
+            else 
+                start=mid+1;
+            
         }
         return ans;
     }
