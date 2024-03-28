e93b6c42980861a089e44d8dc64b6476201eb375
PRAGYA BISHERWAL <90054472+pragyabisherwal@users.noreply.github.com>
2024-03-28 08:18:38 +0530

    Time: 0 ms (100.00%), Space: 41 MB (62.75%) - LeetHub

---
+++ 1752-check-if-array-is-sorted-and-rotated/1752-check-if-array-is-sorted-and-rotated.java
@@ -0,0 +1,24 @@
+class Solution {
+    public boolean check(int[] nums) {
+        
+        int irregularities = 0;
+        int length = nums.length;
+
+        for (int i = 0; i < length; i++) {
+
+            int current = nums[i];
+            int nextPivoted = nums[(i + 1) % length];
+            
+            if (current > nextPivoted) {
+                irregularities += 1;
+                
+                if (irregularities >= 2) 
+                {
+                    return false;
+                }
+            }
+        }
+
+        return true;
+    }
+}
