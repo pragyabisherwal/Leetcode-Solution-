95684ac9a513774747e1e25a04ea4e9c1b9e5d56
PRAGYA BISHERWAL <90054472+pragyabisherwal@users.noreply.github.com>
2024-04-18 12:46:52 +0530

    Time: 25 ms (51.51%), Space: 44.5 MB (97.38%) - LeetHub

--- 0875-koko-eating-bananas/0875-koko-eating-bananas.java
+++ 0875-koko-eating-bananas/0875-koko-eating-bananas.java
@@ -1,47 +1,31 @@
 class Solution {
-    public int checkTime(int[] piles, int maxpiles)
-    {
-        int hours=0;
-        
-        for(int pile : piles)
-        {
-            hours+=pile/maxpiles;
-            
-            if(pile%maxpiles!=0)
-                hours++;
+    public int minEatingSpeed(int[] piles, int h) {
+        int max = Integer.MIN_VALUE;
+        for (int pile : piles) {
+            max = Math.max(max, pile);
         }
-        
-        return hours; 
-    }
-    public int minEatingSpeed(int[] piles, int h) 
-    {
-        int n = piles.length;
-        int low=1;
-        int high=Integer.MIN_VALUE;
-        int ans = -1;
-        
-        for(int i=0;i<piles.length;i++){
-            high=Math.max(high,piles[i]);
-        }
-        
-        while(low<high)
-        {
-            int mid = low + (high-low)/2;
-            
-            int reqTime = checkTime(piles , mid);
-            
-            if(reqTime<=h)
-            {
-                ans = mid;
-                high=mid;
-            }
-            
-            else
-                low = mid+1;
-                
-        }
-        
-        return low;
-    }
+        int start = 1;
+        int end =  max;
     
+        while(start<end){
+            int mid = start + (end-start) / 2;
+            if(koko(mid,piles)>h){
+                start = mid + 1;
+          }
+            else{
+                end=mid;
+            }
+        }
+        return start;
+    }
+
+
+    public int koko(long num, int[] arr){
+    int sum = 0;
+    for(int i = 0; i < arr.length; i++){
+        sum += Math.ceil((double)arr[i]/num);
+    }
+    return sum;
+}
+
 }
