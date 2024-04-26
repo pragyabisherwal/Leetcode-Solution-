7916fe2b3c06d2bf1ecb2e4b7f86401c95d4551c
PRAGYA BISHERWAL <90054472+pragyabisherwal@users.noreply.github.com>
2024-04-26 12:55:19 +0530

    Time: 11 ms (76.02%), Space: 47.1 MB (43.35%) - LeetHub

--- 1011-capacity-to-ship-packages-within-d-days/1011-capacity-to-ship-packages-within-d-days.java
+++ 1011-capacity-to-ship-packages-within-d-days/1011-capacity-to-ship-packages-within-d-days.java
@@ -1,49 +1,38 @@
 class Solution {
-    public boolean isPossible(int weight,int[] weights, int days)
-    {
-        int totalW=0;
-        int totalD=1;
-        
-        for(int num : weights)
-        {
-            if(totalW+num<=weight)
-                totalW+=num;
-            else
-            {
-                totalD++;
-                totalW=num;        
+    public int shipWithinDays(int[] weights, int days) {
+        int sum = 0;
+        int max = weights[0];
+        for (int i = 0; i < weights.length; i++) {
+            sum += weights[i];
+            if (max < weights[i]) {
+                max = weights[i];
             }
-            
-            if(totalD>days)
-                return false;
         }
-        
-        return true;
+        int start = max; // Minimum possible weight
+        int end = sum;   // Maximum possible weight
+        while (start < end) {
+            int mid = start + (end - start) / 2;
+            int minDay = daysRequired(weights, mid);
+            if (minDay <= days) {
+                end = mid;
+            } else {
+                start = mid + 1;
+            }
+        }
+        return start;
     }
-    public int shipWithinDays(int[] weights, int days)
-    {
-        int n = weights.length;
-        int low=Integer.MIN_VALUE;
-        int high=0;
-        
-        for(int num : weights)
-        {
-            low = Math.max(num , low);
-            high+=num;
+
+    private int daysRequired(int[] weights, int capacity) {
+        int days = 1;
+        int currentLoad = 0;
+        for (int weight : weights) {
+            if (currentLoad + weight <= capacity) {
+                currentLoad += weight;
+            } else {
+                days++;
+                currentLoad = weight;
+            }
         }
-        
-        while(low<=high)
-        {
-            int mid = low + (high-low)/2;
-            
-            if(isPossible(mid , weights , days))
-                high = mid-1;
-            
-            else
-                low=mid+1;
-        }
-        
-        return low;
-            
+        return days;
     }
 }
