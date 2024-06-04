d890af58f0af2bd126e3a23a0c1c2728e0bd9439
PRAGYA BISHERWAL <90054472+pragyabisherwal@users.noreply.github.com>
2024-06-04 19:13:35 +0530

    Time: 1 ms (100.00%), Space: 46.1 MB (28.00%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.java
+++ 0283-move-zeroes/0283-move-zeroes.java
@@ -5,11 +5,14 @@ public void moveZeroes(int[] nums) {
     int j = 0;
     for(int i = 0; i < nums.length; i++) {
         if(nums[i] != 0) {
-            int temp = nums[j];
             nums[j] = nums[i];
-            nums[i] = temp;
             j++;
         }
+    }
+    
+    for(int k=j; k<nums.length; k++)
+    {
+        nums[k]=0;
     }
 }
 }
