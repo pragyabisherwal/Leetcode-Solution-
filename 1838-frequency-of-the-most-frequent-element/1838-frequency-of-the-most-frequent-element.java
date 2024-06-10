7713112692a9a720876019200b3b6d468f291feb
PRAGYA BISHERWAL <90054472+pragyabisherwal@users.noreply.github.com>
2024-06-10 12:13:01 +0530

    Time: 30 ms (52.66%), Space: 56.8 MB (42.71%) - LeetHub

--- 1838-frequency-of-the-most-frequent-element/1838-frequency-of-the-most-frequent-element.java
+++ 1838-frequency-of-the-most-frequent-element/1838-frequency-of-the-most-frequent-element.java
@@ -1,11 +1,11 @@
 public class Solution {
     public int maxFrequency(int[] nums, int k) {
         Arrays.sort(nums);
-        
         int n = nums.length;
-        
         int result = 0;
+        //Checkout leetcode notes of this question 
         
+//         //NOTES      ---------------------------------------------------->>>>>>>>>
         int i = 0;
         long currSum = 0;
         
@@ -24,52 +24,40 @@ public class Solution {
         }
         return result;
     }
-}
-
-
+}   
+//FOR EXPLANATION |||||  this will not pass due to some long int conversion issue.But is good to understand how code works
+                    
 // class Solution {
 //     public int maxFrequency(int[] nums, int k) {
-//         //Checkout leetcode notes of this question 
-        
-//         //NOTES
-        
-        
-//         //---------------------------------------------------->>>>>>>>>
 //         Arrays.sort(nums);
-//         int left=0 , right=0;
-//         long current_sum=0;
-         
-//         long window_sum=0 , original_sum=0 , operations=0;
+//         int left = 0, right = 0;
+//         long current_sum = 0 , original_sum = 0;
+
+//         int window_sum = 0,operations = 0;
 //         int result = 0;
-//         while(right<nums.length)
-//         {
-//             long target=nums[right];
-//             current_sum+=nums[right];
-            
-//             //If all the elements are equal to target
-//             //no. of elements in window*target
-//             window_sum = (right-left+1)*target;
+//         for (;right < nums.length;right++) {
+//             long target = nums[right];
+//             current_sum += nums[right];
+
+//             window_sum = (right - left + 1)*(int) target;
 //             original_sum = current_sum;
-            
-//             operations =window_sum - original_sum;
-            
-//             if(operations>k)
-//             {
-//                 //shrink the window
-//                 current_sum-=nums[left];
+
+//             operations = (int)(window_sum - original_sum);
+
+//             if (operations > k) {
+//                 current_sum -= nums[left];
 //                 left++;
-                
-//                 window_sum = (right-left+1)*target;
+
+//                 window_sum = (right - left + 1)*(int)target;
 //                 original_sum = current_sum;
-            
-//                 operations =window_sum - original_sum;
+
+//             operations = (int)(window_sum - original_sum);
 //             }
+
+//             result = Math.max(result, right - left + 1);
             
-//             result = Math.max(result , right-left+1);
 //         }
-        
+
 //         return result;
 //     }
 // }
-
-
