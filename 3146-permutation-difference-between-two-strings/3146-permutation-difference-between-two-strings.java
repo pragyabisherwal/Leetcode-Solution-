b7265d46d789ec826e48c31c146af9247f09e0f8
PRAGYA BISHERWAL <90054472+pragyabisherwal@users.noreply.github.com>
2024-07-01 20:47:32 +0530

    Time: 1 ms (100.00%), Space: 42.6 MB (42.89%) - LeetHub

--- 3146-permutation-difference-between-two-strings/3146-permutation-difference-between-two-strings.java
+++ 3146-permutation-difference-between-two-strings/3146-permutation-difference-between-two-strings.java
@@ -1,19 +1,28 @@
 class Solution {
     public int findPermutationDifference(String s, String t) 
     {
-        HashMap<Character , Integer> hm = new HashMap<>();
-        int sum = 0;
-        
-        for(int i=0 ; i<t.length(); i++)
-            hm.put(t.charAt(i) , i);
-        
-        for(int i=0 ; i<s.length() ; i++)
-        {
-            int ele1 = hm.get(s.charAt(i));
-            sum = sum + Math.abs(ele1 - i);
-            
+        ///BEST SOLUTION
+        int res = 0;
+        for(int i=0; i<s.length(); i++){
+            res += Math.abs(i-t.indexOf(s.charAt(i)));
         }
+        return res;
         
-        return sum;
+        
+        ///SECOND BEST SOLUTION
+//         HashMap<Character , Integer> hm = new HashMap<>();
+//         int sum = 0;
+        
+//         for(int i=0 ; i<t.length(); i++)
+//             hm.put(t.charAt(i) , i);
+        
+//         for(int i=0 ; i<s.length() ; i++)
+//         {
+//             int ele1 = hm.get(s.charAt(i));
+//             sum = sum + Math.abs(ele1 - i);
+            
+//         }
+        
+//         return sum;
     }
 }
