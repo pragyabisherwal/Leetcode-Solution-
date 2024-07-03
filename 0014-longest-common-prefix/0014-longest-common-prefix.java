683ee2efa97637c120693da91800a89b59002dc5
PRAGYA BISHERWAL <90054472+pragyabisherwal@users.noreply.github.com>
2024-07-03 13:35:19 +0530

    Time: 0 ms (100.00%), Space: 41.3 MB (55.23%) - LeetHub

--- 0014-longest-common-prefix/0014-longest-common-prefix.java
+++ 0014-longest-common-prefix/0014-longest-common-prefix.java
@@ -5,11 +5,18 @@ class Solution {
             return strs[0];
         
         String prefix=strs[0];
-        
+        //System.out.println(prefix);
         for(int i=1 ; i<strs.length ; i++)
         {
+            
+            //When prefix string size length is zero indexOf function return 0 so in that case we need to break the while loop so that we can return answer as an empty string that's why we have condition strs[i].indexOf(prefix)!="0"
+            
             while(strs[i].indexOf(prefix)!=0)
+            {
+                //System.out.println(strs[i].indexOf(""));
                 prefix = prefix.substring(0 , prefix.length()-1);
+            }
+            
             
             if(prefix.length()==0)
                 return "";
